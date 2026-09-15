5309183d0607be4d1154c60446ec08f84171eb6c
Dylan Beattie <dylan@dylanbeattie.net>
2026-09-15 16:32:47 +0200

    Remove Aspire for services we're actually going to run for real

--- Autobarn/Autobarn.AppHost/AppHost.cs
+++ Autobarn/Autobarn.AppHost/AppHost.cs
@@ -2,16 +2,16 @@ using Autobarn.ServiceDefaults;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var rabbitMqUsername = builder.AddParameter("username", "admin", secret: true);
-var rabbitMqPassword = builder.AddParameter("password", "secret", secret: true);
+//var rabbitMqUsername = builder.AddParameter("username", "admin", secret: true);
+//var rabbitMqPassword = builder.AddParameter("password", "secret", secret: true);
 
-var rabbitmq = builder.AddRabbitMQ(
-		"rabbitmq",
-		userName: rabbitMqUsername,
-		password: rabbitMqPassword)
-	.WithContainerName("autobarn-rabbitmq")
-	.WithLifetime(ContainerLifetime.Persistent)
-	.WithManagementPlugin();
+//var rabbitmq = builder.AddRabbitMQ(
+//		"rabbitmq",
+//		userName: rabbitMqUsername,
+//		password: rabbitMqPassword)
+//	.WithContainerName("autobarn-rabbitmq")
+//	.WithLifetime(ContainerLifetime.Persistent)
+//	.WithManagementPlugin();
 
 // var pricingServer = builder
 // 	// The "http" profile keeps this off https://localhost:5003, which belongs to the Node.js pricing server.
@@ -33,32 +33,30 @@ var nodePricingServer = builder
 	});
 #pragma warning restore ASPIRECERTIFICATES001
 
-builder.AddProject<Projects.Autobarn_AuditLog>("auditlog")
-	.WithReference(rabbitmq)
-	.WaitFor(rabbitmq);
+builder.AddProject<Projects.Autobarn_AuditLog>("auditlog");
+	//.WithReference(rabbitmq)
+	//.WaitFor(rabbitmq);
 
 var website = builder.AddProject<Projects.Autobarn_Website>("autobarn-website")
-	.WaitFor(rabbitmq)
-	.WithReference(rabbitmq)
+	//.WaitFor(rabbitmq)
+	//.WithReference(rabbitmq)
 	.WithHttpEndpoint(name: ConfigKeys.AutobarnWebsiteUrl);
 
-builder.AddProject<Projects.Autobarn_Notifier>("notifier")
-	.WithReference(rabbitmq)
-	.WaitFor(rabbitmq)
-	.WithReference(website)
-	.WithEnvironment(ConfigKeys.AutobarnWebsiteUrl, website.GetEndpoint(ConfigKeys.AutobarnWebsiteUrl))
-	.WaitFor(website);
+//builder.AddProject<Projects.Autobarn_Notifier>("notifier");
+	//.WithReference(rabbitmq)
+	//.WaitFor(rabbitmq)
+	//.WithReference(website)
+	//.WithEnvironment(ConfigKeys.AutobarnWebsiteUrl, website.GetEndpoint(ConfigKeys.AutobarnWebsiteUrl))
+	//.WaitFor(website);
 
-builder.AddProject<Projects.Autobarn_PricingClient>("pricing-client")
-	.WithReference(rabbitmq)
-	.WaitFor(rabbitmq)
-	.WithReference(nodePricingServer)
-	.WaitFor(nodePricingServer)
-	.WithEnvironment(
-		ConfigKeys.GrpcPricingServerUrl,
-		nodePricingServer.GetEndpoint("https"));
-	// pricingServer.GetEndpoint(ConfigKeys.GrpcPricingServerUrl));
-
-
+//builder.AddProject<Projects.Autobarn_PricingClient>("pricing-client")
+//	//.WithReference(rabbitmq)
+//	//.WaitFor(rabbitmq)
+//	.WithReference(nodePricingServer)
+//	.WaitFor(nodePricingServer)
+//	.WithEnvironment(
+//		ConfigKeys.GrpcPricingServerUrl,
+//		nodePricingServer.GetEndpoint("https"));
+//	// pricingServer.GetEndpoint(ConfigKeys.GrpcPricingServerUrl));
 
 builder.Build().Run();
